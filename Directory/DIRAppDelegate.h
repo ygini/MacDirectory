61764c8c42849dd65ed3c82e4f335d2dfc9bd3df
Yoann Gini <yoann.gini@gmail.com>
2013-09-03 18:36:54 +0200

    Initial Commit

---
+++ Directory/DIRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DIRAppDelegate.h
+//  Directory
+//
+//  Created by Yoann Gini on 03/09/13.
+//  Copyright (c) 2013 Yoann Gini. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DIRAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Directory/DIRAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  DIRAppDelegate.m
+//  Directory
+//
+//  Created by Yoann Gini on 03/09/13.
+//  Copyright (c) 2013 Yoann Gini. All rights reserved.
+//
+
+#import "DIRAppDelegate.h"
+
+@implementation DIRAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
